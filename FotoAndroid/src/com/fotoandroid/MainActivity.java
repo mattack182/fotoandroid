992c9972e8650d07fcf95defb98ee4eacbcc18c4
mattack <eng_ufsc@hotmail.com>
2013-08-01 17:10:29 -0300

    alteracoes no layout

--- FotoAndroid/src/com/fotoandroid/MainActivity.java
+++ FotoAndroid/src/com/fotoandroid/MainActivity.java
@@ -4,25 +4,32 @@ import java.io.File;
 import java.util.ArrayList;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+// imports do projeto
 
 public class MainActivity extends Activity {
-	public int numFotos;
+	public int counter;
 	public File f;
-	ArrayList<Fotos> itens = new ArrayList<MainActivity.Fotos>();
-	
+	Adaptador adaptador;
+	ArrayList<Foto> foto = new ArrayList<Foto>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		counter = 0;
 	}
 
 	@Override
@@ -32,26 +39,66 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	public void capturaFoto(View v){
-		f = new File(Environment.getExternalStorageDirectory(),"foto"+numFotos+".jpg");
+	
+	class Adaptador extends BaseAdapter {
+		Context context;
+		LayoutInflater inflater;
+
+		public Adaptador(Context context) {
+			this.context = context;
+			inflater = (LayoutInflater) context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		}
+
+		@Override
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			// TODO Auto-generated method stub
+			return 0;
+		}
+
+		@Override
+		public View getView(int arg0, View arg1, ViewGroup arg2) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+	}
+	
+	public void capturaFoto(View v) {
+		f = new File(Environment.getExternalStorageDirectory(), "img" + counter
+				+ ".jpg");
 		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 		intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
 		intent.putExtra("return-data", false);
-		startActivityForResult(intent, 100);
+		startActivityForResult(intent, 80);
 	}
-	
-	// Classe que ira definir a minha lista
-		class Fotos {
-			public Bitmap thumb; // bitmap para a imagem reduzida
-			public String nome; // nome da imagem (para mostrar no ListView(firula..)
-			public String url; // Url Completa da Imagem :) para ser aberta na Activity PhotoEdit
 
-			public Fotos(Bitmap thumb,String nome,String url) {
-				// TODO Auto-generated constructor stub
-				this.thumb = thumb;
-				this.nome = nome;
-				this.url = url;
-			}		
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		// TODO Auto-generated method stub
+		// super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == 80 && resultCode == RESULT_OK) {
+			Bitmap foto_bmp = null;
+			try {
+				foto_bmp = BitmapFactory.decodeFile(f.getAbsolutePath());
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			foto.add(new Foto(foto_bmp, f.getName(), f.getAbsolutePath()));
 		}
+
+	}
 
 }
